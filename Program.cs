4842da2bbb8b65c25d3c141fa81388959514be1a
Snehasrg <sghongade642@gmail.com>
2023-03-17 17:17:51 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(String[] args)
         {
-            EmpAttendance.EmpComputation();
+            // EmpAttendance.EmpComputation();
+            EmpDailyWage.EmpComputation();
         }
     }
 }
